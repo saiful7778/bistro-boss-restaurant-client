62779e1f930f61bfcb2330af97e560fa3b1e4c6d
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-28 13:04:45 +0600

    add base container style

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">Main Layout</h1>
-      <main>
-        <Outlet />
-      </main>
+    <div className="font-inter relative min-h-screen w-full overflow-x-hidden bg-gray-50 text-gray-900 duration-300">
+      <div className="container p-2">
+        <h1 className="text-3xl font-bold">Main Layout</h1>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 };
