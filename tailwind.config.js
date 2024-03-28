8ee3c1abca11d907f4c9d3a252a72e97434da1a7
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-29 01:44:05 +0600

    add cinnzel font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+import keepPreset from "keep-react/preset";
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [keepPreset],
   theme: {
     extend: {
       container: {
         center: true,
       },
+      colors: {
+        "dark-01": "#151515",
+        accent: "#EEFF25",
+      },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+        cinzel: ["Cinzel", "serif"],
       },
     },
   },
