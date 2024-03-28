dd99f145e29c5ce5f7b30e412668325dfdf26d26
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-28 13:04:22 +0600

    add google inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
