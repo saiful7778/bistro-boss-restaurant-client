87ada67a677efb32a10416d26514d0f4fe2f8342
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-30 14:28:07 +0600

    add server link

--- src/libs/axios.config.js
+++ src/libs/axios.config.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5001",
+  // baseURL: "http://localhost:5001",
+  baseURL: "https://bistro-boss-restaurant-server-eosin.vercel.app",
 });
 
 export default axiosBase;
